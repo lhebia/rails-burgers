80bea9f03d72ef9ed798bbf3ff66601cff8b7d53
lhebia <lhebia@gmail.com>
2021-08-11 00:12:02 -0400

    Update associations for review and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :reviews
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :username, 
     presence: true,
